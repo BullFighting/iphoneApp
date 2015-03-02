8ccd7555aa2bffe6431dd927739e1b7540f36ce1
yumikokh <ymymyummy@gmail.com>
2015-03-02 14:20:03 +0900

    ？

---
+++ ImageCaptureSample/foundRed.h
@@ -0,0 +1,22 @@
+//
+//  foundRed.h
+//  ImageCaptureSample
+//
+//  Created by Yumiko Kokubu on 2014/12/13.
+//  Copyright (c) 2014年 Olympus Imaging Corporation. All rights reserved.
+//
+
+#ifndef __ImageCaptureSample__foundRed__
+#define __ImageCaptureSample__foundRed__
+
+#include <stdio.h>
+
+class foundRed {
+public:
+    void initialize();
+    
+    void getImg();
+    
+};
+
+#endif /* defined(__ImageCaptureSample__foundRed__) */
